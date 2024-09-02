e86e17eb78495c538dbd0571e4ef078334ce109e
snehagittt <snehasaha722004@gmail.com>
2024-09-02 18:48:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// main.js
+var typed = new Typed('.text', {
+    strings: ['Frontend Developer', 'Web Developer','Web Designing'],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay:1000,
+  });
+ 
+  
+
+
